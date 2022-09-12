b898fa28eaa319de7b77d1ab4b1e3d1c9fd5f4c0
cw1322311203 <1322311203@qq.com>
2022-09-12 20:40:40 +0800

    add

--- src/main/java/com/cw/flink/chapter07/TopNExample.java
+++ src/main/java/com/cw/flink/chapter07/TopNExample.java
@@ -6,15 +6,25 @@ import com.cw.flink.chapter06watermark.window.UrlCountViewExample;
 import com.cw.flink.chapter06watermark.window.UrlViewCount;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
+import java.sql.Timestamp;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
- * @description:
+ * @description: 使用 KeyedProcessFunction实现TOPN
  * @author: chenwei
  * @date: 2022/9/9 16:33
  */
@@ -44,8 +54,75 @@ public class TopNExample {
         urlCountStream.print("url count");
 
         // 2.对于同一窗口统计出的访问量，进行收集和排序
-
+        urlCountStream.keyBy(data -> data.windowEnd)
+                .process(new TopNProcessResult(2))
+                .print();
 
         env.execute();
+    }
+
+    // 实现自定义KeyedProcessFunction
+    public static class TopNProcessResult extends KeyedProcessFunction<Long, UrlViewCount, String> {
+        // 定义一个属性，n
+        private Integer n;
+        // 定义列表状态
+        private ListState<UrlViewCount> urlViewCountListState;
+
+        public TopNProcessResult(Integer n) {
+            this.n = n;
+        }
+
+        // 在环境中获取状态
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            // 从环境中获取列表状态句柄
+            urlViewCountListState = getRuntimeContext().getListState(
+                    new ListStateDescriptor<UrlViewCount>("url-count-list", Types.POJO(UrlViewCount.class))
+            );
+        }
+
+        @Override
+        public void processElement(UrlViewCount urlViewCount, Context context, Collector<String> collector) throws Exception {
+            // 将数据保存到状态中
+            urlViewCountListState.add(urlViewCount);
+            // 注册windowEnd + 1ms的定时器
+            context.timerService().registerEventTimeTimer(context.getCurrentKey() + 1);
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+            ArrayList<UrlViewCount> urlViewCountArrayList = new ArrayList<>();
+
+            for (UrlViewCount urlViewCount : urlViewCountListState.get()) {
+                urlViewCountArrayList.add(urlViewCount);
+            }
+
+            // 排序
+            urlViewCountArrayList.sort(new Comparator<UrlViewCount>() {
+                @Override
+                public int compare(UrlViewCount o1, UrlViewCount o2) {
+                    return o2.count.intValue() - o1.count.intValue();
+                }
+            });
+
+            // 包装信息打印输出
+            StringBuilder result = new StringBuilder();
+            result.append("------------------------------\n");
+            result.append("窗口结束时间: " + new Timestamp(ctx.getCurrentKey()) + "\n");
+
+
+            // 取List前两个，包装信息输出
+            for (int i = 0; i < 2; i++) {
+                UrlViewCount currTuple = urlViewCountArrayList.get(i);
+                String info = "No. " + (i + 1) + " "
+                        + "url: " + currTuple.url + " "
+                        + "访问量: " + currTuple.count + " \n";
+                result.append(info);
+
+            }
+            result.append("------------------------------\n");
+
+            out.collect(result.toString());
+        }
     }
 }
--- src/main/java/com/cw/flink/chapter07/TopNExample_ProcessAllWindowFunction.java
+++ src/main/java/com/cw/flink/chapter07/TopNExample_ProcessAllWindowFunction.java
@@ -8,15 +8,21 @@ import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
 /**
- * @description:
+ * @description: 使用 ProcessAllWindowFunction统计访问量前两名的url
+ *
  * @author: chenwei
  * @date: 2022/9/9 17:10
  */
@@ -66,12 +72,50 @@ public class TopNExample_ProcessAllWindowFunction {
 
         @Override
         public ArrayList<Tuple2<String, Long>> getResult(HashMap<String, Long> accumulator) {
-            return null;
+            ArrayList<Tuple2<String, Long>> result = new ArrayList<>();
+            for (String key : accumulator.keySet()) {
+                result.add(Tuple2.of(key, accumulator.get(key)));
+            }
+            result.sort(new Comparator<Tuple2<String, Long>>() {
+                @Override
+                public int compare(Tuple2<String, Long> o1, Tuple2<String, Long> o2) {
+                    return o2.f1.intValue() - o1.f1.intValue();
+                }
+            });
+
+            return result;
         }
 
         @Override
         public HashMap<String, Long> merge(HashMap<String, Long> a, HashMap<String, Long> b) {
             return null;
+        }
+    }
+
+    // 实现自定义全窗口函数，包装信息输出结果
+    public static class UrlAllWindowResult extends ProcessAllWindowFunction<ArrayList<Tuple2<String, Long>>, String, TimeWindow> {
+
+        @Override
+        public void process(Context context, Iterable<ArrayList<Tuple2<String, Long>>> iterable, Collector<String> collector) throws Exception {
+            ArrayList<Tuple2<String, Long>> list = iterable.iterator().next();
+
+            StringBuilder result = new StringBuilder();
+            result.append("------------------------------\n");
+            result.append("窗口结束时间: " + new Timestamp(context.window().getEnd()) + "\n");
+
+
+            // 取List前两个，包装信息输出
+            for (int i = 0; i < 2; i++) {
+                Tuple2<String, Long> currTuple = list.get(i);
+                String info = "No. " + (i + 1) + " "
+                        + "url: " + currTuple.f0 + " "
+                        + "访问量: " + currTuple.f1 + " \n";
+                result.append(info);
+
+            }
+            result.append("------------------------------\n");
+
+            collector.collect(result.toString());
         }
     }
 
