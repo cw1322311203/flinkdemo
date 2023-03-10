f064c592fc1f47d1147a698e6268dc77a2c1ba4a
chenwei <ys.chenweid@h3c.com>
2023-03-10 17:08:02 +0800

    add

--- src/main/java/com/cw/flink/chapter06watermark/watermark/CustomWatermarkTest_02_02Punctuated.java
+++ src/main/java/com/cw/flink/chapter06watermark/watermark/CustomWatermarkTest_02_02Punctuated.java
@@ -6,29 +6,29 @@ import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * 自定义水位线的产生
+ * TODO 自定义水位线的产生（断点式生成水位线）
+ * 在 WatermarkStrategy 中，时间戳分配器 TimestampAssigner 都是大同小异的，指定字段提取时间戳就可以了；
+ * 而不同策略的关键就在于 WatermarkGenerator 的实现。
+ * 整体说来，Flink有两种不同的生成水位线的方式：一种是周期性的（Periodic），另一种是断点式的（Punctuated）。
+ * <p>
+ * WatermarkGenerator 接口中有两个方法onEvent()和 onPeriodicEmit()，
+ * 前者是在每个事件到来时调用，而后者由框架周期性调用。周期性调用的方法中发出水位线，自然就是周期性生成水位线；
+ * 而在事件触发的方法中发出水位线，自然就是断点式生成了。两种方式的不同就集中体现在这两个方法的实现上。
  *
  * @author:chenwei
  * @date:2022/9/8 15:12
  */
-public class CustomWatermarkTest_02 {
+public class CustomWatermarkTest_02_02Punctuated {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         env.addSource(new ClickSource())
-                /**
-                 * assignTimestampsAndWatermarks()方法需要传入一个 WatermarkStrategy 作为参数，这就是所谓的 “水位线生成策略 ” 。
-                 */.assignTimestampsAndWatermarks(new CustomWatermarkStrategy()).print();
+                .assignTimestampsAndWatermarks(new CustomWatermarkStrategy()).print();
 
         env.execute();
     }
 
-    /**
-     * public interface WatermarkStrategy<T> extends TimestampAssignerSupplier<T>, WatermarkGeneratorSupplier<T>
-     * <p>
-     * WatermarkStrategy中包含了一个“时间戳分配器”TimestampAssigner和一个“水位线生成器”WatermarkGenerator
-     */
     public static class CustomWatermarkStrategy implements WatermarkStrategy<Event> {
         /**
          * TimestampAssigner：主要负责从流中数据元素的某个字段中提取时间戳，并分配给元素。时间戳的分配是生成水位线的基础。
@@ -55,7 +55,6 @@ public class CustomWatermarkTest_02 {
 
     public static class CustomPeriodicGenerator implements WatermarkGenerator<Event> {
         private Long delayTime = 5000L; // 延迟时间
-        private Long maxTs = Long.MIN_VALUE + delayTime + 1L; // 观察到的最大时间戳
 
         /**
          * onEvent：每个事件（数据）到来都会调用的方法，它的参数有当前事件、时间戳， 以及允许发出水位线的一个 WatermarkOutput，可以基于事件做各种操作
@@ -67,7 +66,17 @@ public class CustomWatermarkTest_02 {
         @Override
         public void onEvent(Event event, long eventTimestamp, WatermarkOutput output) {
             // 每来一条数据就调用一次
-            maxTs = Math.max(event.timestamp, maxTs); // 更新最大时间戳
+            // 只有在遇到特定的 itemId 时，才发出水位线
+            /**
+             * onEvent()中判断当前事件的 user 字段，只有遇到“Mary”这个特殊的值时，才调用output.emitWatermark()发出水位线。
+             * 这个过程是完全依靠事件来触发的，所以水位线的生成一定在某个数据到来之后。
+             */
+            if (event.user.equals("Mary")) {
+                output.emitWatermark(new Watermark(event.timestamp + delayTime - 1));
+                // 输出当前水位线
+                System.out.println(new Watermark(event.timestamp + delayTime - 1));
+            }
+
         }
 
         /**
@@ -79,8 +88,7 @@ public class CustomWatermarkTest_02 {
          */
         @Override
         public void onPeriodicEmit(WatermarkOutput output) {
-            // 发射水位线，默认200ms调用一次
-            output.emitWatermark(new Watermark(maxTs - delayTime - 1L));
+            // 不需要做任何事情，因为我们在 onEvent 方法中发射了水位线
         }
     }
 }
--- src/main/java/com/cw/flink/chapter06watermark/watermark/EmitWatermarkInSourceFunction_03.java
+++ src/main/java/com/cw/flink/chapter06watermark/watermark/EmitWatermarkInSourceFunction_03.java
@@ -10,12 +10,14 @@ import java.util.Calendar;
 import java.util.Random;
 
 /**
- * 在自定义数据源中发送水位线
+ * TODO 在自定义数据源中发送水位线
+ * 在自定义水位线中生成水位线相比 assignTimestampsAndWatermarks 方法更加灵活，可以任意的产生周期性的、非周期性的水位线，
+ * 以及水位线的大小也完全由我们自定义。所以非常适合用来编写 Flink 的测试程序，测试 Flink 的各种各样的特性。
  *
  * @author:chenwei
  * @date:2022/9/8 15:14
  */
-public class EmitWatermarkInSourceFunction {
+public class EmitWatermarkInSourceFunction_03 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -39,8 +41,13 @@ public class EmitWatermarkInSourceFunction {
                 String username = userArr[random.nextInt(userArr.length)];
                 String url = urlArr[random.nextInt(urlArr.length)];
                 Event event = new Event(username, url, currTs);
-                // 使用collectWithTimestamp方法将数据发送出去，并指明数据中的时间戳的字段
+
+                /**
+                 使用collectWithTimestamp方法将数据发送出去，并指明数据中的时间戳的字段
+                 sourceContext.collect(Event event)：只发送数据
+                 */
                 sourceContext.collectWithTimestamp(event, event.timestamp);
+
                 // 发送水位线
                 sourceContext.emitWatermark(new Watermark(event.timestamp - 1L));
                 Thread.sleep(1000L);
